dd48cd1e1795cdf0ed4cd543678e4f207c71618b
Leonardo Dionizio <leodionizio13@gmail.com>
2020-08-05 14:19:20 -0300

    improving navbar component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,13 +6,17 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { InMemoryDatabase } from "../in-memory-database";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
   ],
@@ -21,6 +25,9 @@ import { InMemoryDatabase } from "../in-memory-database";
     BrowserAnimationsModule,
     HttpClientModule,
     HttpClientModule,
+    RouterModule,
+    // exports components
+    NavbarComponent
   ],
 })
 export class CoreModule {}
