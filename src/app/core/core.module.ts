0b527507d8cd721b18a2bc11b7a14e47f6b05d2a
Leonardo Dionizio <leodionizio13@gmail.com>
2020-08-05 14:56:14 -0300

    adding footer component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HttpClientModule } from "@angular/common/http";
-
-import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
-import { InMemoryDatabase } from "../in-memory-database";
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDatabase } from '../in-memory-database';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, FooterComponent],
   imports: [
     CommonModule,
     BrowserModule,
@@ -27,7 +28,8 @@ import { RouterModule } from '@angular/router';
     HttpClientModule,
     RouterModule,
     // exports components
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
   ],
 })
 export class CoreModule {}
